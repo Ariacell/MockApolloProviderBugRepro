53222487488c1839a858e92e4f5572c9ca972022
Aria Jackson <aria.jackson@outlook.com.au>
2020-09-07 20:58:33 +1000

    Added workspace support for running server on 5000 and web on 3000

--- workspaces/server/src/server.ts
+++ workspaces/server/src/server.ts
@@ -13,5 +13,5 @@ const server = new ApolloServer({
 });app.use('*', cors());
 app.use(compression());
 server.applyMiddleware({ app, path: '/graphql' });const httpServer = createServer(app);httpServer.listen(
-  { port: 3000 },
-  (): void => console.log(`\n🚀      GraphQL is now running on http://localhost:3000/graphql`));
+  { port: 5000 },
+  (): void => console.log(`\n🚀      GraphQL is now running on http://localhost:5000/graphql`));
